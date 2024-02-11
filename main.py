823798572545fa54718bb783e1068d9c8c71a0b1
Tan05 <tan.tantham.tan@gmail.com>
2024-02-11 22:50:43 +0700

    doesnt friggin work im gonna go jump off a building

--- main.py
+++ main.py
@@ -3,7 +3,37 @@ import random
 
 useCols = ["Word"]
 num = random.randint(0, 13161)
+ingame = True
 
 words = pd.read_csv("word-meaning-examples.csv", index_col=0, usecols=useCols)
 
+wordArr = []
+
+for i in words.index[num]:
+    wordArr.append([i, "_"])
+
 print(words.index[num])
+
+usrAns = ""
+
+while ingame:
+    string = ""
+    for i in wordArr:
+        string += i[1] + " "
+        usrAns += i[1]
+
+    print(string)
+    usrInpt = input(">>")
+
+    for i in wordArr:
+        if i[0] == usrInpt:
+            i[1] = i[0]
+
+    completeString = ""
+
+    for i in wordArr:
+        completeString += i[0]
+
+    if string == words.index[num]:
+        print("You won")
+        break
