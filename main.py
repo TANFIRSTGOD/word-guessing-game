07f536cdb952ccc581736313e6e895f521b2ff62
Tan05 <tan.tantham.tan@gmail.com>
2024-02-11 20:34:06 +0700

    generate a random word

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import random
 
 useCols = ["Word"]
+num = random.randint(0, 13161)
 
 words = pd.read_csv("word-meaning-examples.csv", index_col=0, usecols=useCols)
 
-for i in words.index:
-    print(i)
+print(words.index[num])
